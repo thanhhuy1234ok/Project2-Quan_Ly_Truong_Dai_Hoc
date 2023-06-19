3550691fd83cc23c53d7c285671f810828b9ec41
thanhhuy <nguyenvothanhhuy2002@gmail.com>
2023-06-19 15:00:32 +0700

    ADD sass

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const port = 3000;
 const handlebars = require('express-handlebars');
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 //* Template engine
 app.engine(
    'hbs',
@@ -15,9 +17,14 @@ app.engine(
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+
 app.get('/', (req, res) => {
   res.render('home');
 })
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
